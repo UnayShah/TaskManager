67f739e849b3767ccf62c4a6f255f500230b5429
Unay Shah <unayshah@gmail.com>
2020-09-15 20:07:32 +0530

    Minor code changes and remove unwanted dependencies

--- src/main/java/com/unay/taskmanager/controller/UIController.java
+++ src/main/java/com/unay/taskmanager/controller/UIController.java
@@ -1,15 +1,8 @@
 package com.unay.taskmanager.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller 
 public class UIController {
